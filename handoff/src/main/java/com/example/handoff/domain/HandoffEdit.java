242e1ee3800fe61f4ace68fa1a3cf3fe5d58c057
ryoya07 <ryoya2167@gmail.com>
2026-02-17 09:49:52 +0900

    不足分

---
+++ handoff/src/main/java/com/example/handoff/domain/HandoffEdit.java
@@ -0,0 +1,49 @@
+package com.example.handoff.domain;
+
+import java.time.LocalDateTime;
+
+public class HandoffEdit {
+    private Long id;
+    private String title;
+    private String content;
+    private Long createdBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Long getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Long createdBy) {
+		this.createdBy = createdBy;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
